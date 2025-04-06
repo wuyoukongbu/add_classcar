dda8e3ed2d3c719cd64b0d99a24e1570c66b23df
wuyoukongbu <your.email@example.com>
2025-04-06 14:56:48 +0800

    Fix WSGI configuration for LeanCloud deployment

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,8 @@
 from app import app
+import os
+
+# 确保这个变量名是 application
+application = app
 
 if __name__ == '__main__':
-    app.run() 
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
